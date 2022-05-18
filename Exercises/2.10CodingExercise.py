d5f0bf85d95c54ec2420d766457f8148462b5923
raangeles <raangeles01@gmail.com>
2022-05-18 16:54:33 +0800

    My answers to Exercises for intro to Python

---
+++ Exercises/2.10CodingExercise.py
@@ -0,0 +1,11 @@
+# 1 Create a boolean statement to determine whether 10 is greater than 7
+# 2 Conver the numerical value 73911 into a string.
+# 3 Convert the string "Thank God it's Friday!" into a tuple.
+# 4 Convert 4302 into a floating number
+# 5 Convert 3299.35640 into an integer.
+
+print(bool(10 > 7))
+print(str(73911))
+print(tuple("Thank God it's Friday!"))
+print(float(4302))
+print(int(3299.35640))
